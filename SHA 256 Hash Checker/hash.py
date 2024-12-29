cc730f7851802117c3896001939f763843105cbd
Ayan <ayanbora.co.in@gmail.com>
2024-12-29 14:40:53 -0500

    Input message changed

--- SHA 256 Hash Checker/hash.py
+++ SHA 256 Hash Checker/hash.py
@@ -4,7 +4,7 @@ target = 0x0000000000000000000297fa0000000000000000000000000000000000000000
 num=0
 
 print(target)
-a=input("How much?")
+a=input("How much hashes? ")
 for x in range(int(a)):
    hash = hashlib.sha256(str(num).encode()).hexdigest()
 
@@ -15,16 +15,16 @@ for x in range(int(a)):
    print(hash)
    num += 1
 
-b = input("Continue?")
+b = input("Continue? ")
 
 if b == "yes" :
-   c = input("How much?")
+   c = input("How much hashes? ")
    for x in range(int(c)):
       hash = hashlib.sha256(str(num).encode()).hexdigest()
 
       if int(hash,16) <= target:
-         print("Hash Found!")
-         break
+         print("Hash Found")
+         break 
      
       print(hash)
       num += 1
